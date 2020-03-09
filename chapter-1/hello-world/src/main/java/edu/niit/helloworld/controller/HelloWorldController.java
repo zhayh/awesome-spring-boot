f6837cb94878af2cb0a9e847189f878078d5a045
zhayh <zhayh.nj@gmail.com>
2020-03-09 13:39:34 +0800

    '修改带参数的RESTful请求和测试用例'

--- chapter-1/hello-world/src/main/java/edu/niit/helloworld/controller/HelloWorldController.java
+++ chapter-1/hello-world/src/main/java/edu/niit/helloworld/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package edu.niit.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,7 +18,7 @@ public class HelloWorldController {
 
     // 带参数请求
     @GetMapping("/hello/{name}")
-    public String hello(String name) {
+    public String hello(@PathVariable(name = "name") String name) {
         return "Hello, " + name;
     }
 }
