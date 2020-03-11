6b7eb02b0ef629d4f0bc2952148bb5d8c4d68938
zhayh <zhayh.nj@gmail.com>
2020-03-11 13:47:02 +0800

    增加两个注解的说明

--- chapter-2/props-demo/src/main/java/edu/niit/props/PropsDemoApplication.java
+++ chapter-2/props-demo/src/main/java/edu/niit/props/PropsDemoApplication.java
@@ -1,7 +1,12 @@
 package edu.niit.props;
 
+/**
+ * 如果Application放在某个包下，就需要使用@ComponentScan注解指明扫描包的路径，如
+ *
+ * @ComponentScan(basePackages = {"edu.niit.props"})
+ */
+
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
