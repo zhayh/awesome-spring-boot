c580fa7153e157bc41342c28e070711ef1d73894
zhayh <zhayh.nj@gmail.com>
2020-03-26 13:38:31 +0800

    swagger排除NumberFormat的异常

---
+++ chapter-5/custom-demo/src/main/java/com/spring/restful/custom/common/Result.java
@@ -0,0 +1,49 @@
+package com.spring.restful.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+/**
+ * @author : zhayh
+ * @date : 2020-3-20 12:08
+ * @description : 通用返回值类,统一数据响应的返回结果格式
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result<T> {
+    private int code = ExceptionType.SUCCESS.getCode();
+    private String message = ExceptionType.SUCCESS.getMsg();
+    private T data;
+
+    public static Result<Object> success() {
+        return success(null);
+    }
+
+    public static Result<Object> success(Object data) {
+        Result<Object> result = new Result<>();
+        result.setCode(ExceptionType.SUCCESS.getCode());
+        result.setMessage(ExceptionType.SUCCESS.getMsg());
+        result.setData(data);
+        return result;
+    }
+
+    public static Result<Object> fail(String msg) {
+        return fail(msg,null);
+    }
+
+    public static Result<Object> fail(String msg, Object data) {
+        return fail(ExceptionType.SERVER_ERROR.getCode(), msg,null);
+    }
+
+    public static Result<Object> fail(int code, String msg, Object data) {
+        Result<Object> result = new Result<>();
+        result.setCode(code);
+        result.setMessage(msg);
+        result.setData(data);
+        return result;
+    }
+}
---
+++ chapter-5/custom-demo/src/main/java/com/spring/restful/custom/config/SwaggerConfig.java
@@ -0,0 +1,116 @@
+package com.spring.api.swagger.config;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : zhayh
+ * @date : 2020-3-20 16:03
+ * @description : Swagger配置类
+ */
+@Data
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .groupName("消息管理API")
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .useDefaultResponseMessages(false)
+                .forCodeGeneration(true).select()
+                .apis(RequestHandlerSelectors.basePackage("com.spring.api.swagger.controller"))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    /**
+     * 配置页面展示的基本信息：标题、描述、版本、服务条款、联系方式等
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("消息管理")
+                .description("消息管理 API 操作文档")
+                .termsOfServiceUrl("http://localhost:8080")
+                .contact(new Contact("niit", "http://www.niit.com", "admin@niit.com"))
+                .version("1.0")
+                .build();
+    }
+}
+// 以下是使用配置文件的信息的类定义
+//@Data
+//@Configuration
+//@EnableSwagger2
+//@ConfigurationProperties(prefix="swagger")
+//public class Swagger1Config {
+//    private String title;
+//    private String desc;
+//    private String version;
+//    private String termsOfServiceUrl;
+//    private String license;
+//    private String licenseUrl;
+//    private String basePackage;
+//    private String groupName;
+//    private String contactName;
+//    private String contactUrl;
+//    private String contactEmail;
+//
+//    @Bean
+//    public Docket swaggerApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .groupName(groupName)
+//                .directModelSubstitute(LocalDate.class, String.class)
+//                .genericModelSubstitutes(ResponseEntity.class)
+//                .useDefaultResponseMessages(false)
+//                .globalResponseMessage(RequestMethod.POST, customerResponseMessage())
+//                .globalResponseMessage(RequestMethod.GET, customerResponseMessage())
+//                .forCodeGeneration(true).select()
+//                .apis(RequestHandlerSelectors.basePackage(basePackage))
+//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//    /**
+//     * 配置页面展示的基本信息：标题、描述、版本、服务条款、联系方式等
+//     */
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder().title(title).description(desc)
+//                .version(version).termsOfServiceUrl(termsOfServiceUrl)
+//                .licenseUrl(licenseUrl).license(license)
+//                .contact(new Contact(contactName, contactUrl, contactEmail))
+//                .build();
+//    }
+//
+//    private List<ResponseMessage> customerResponseMessage() {
+//        List<ResponseMessage> list = new ArrayList<>();
+//        list.add(new ResponseMessageBuilder().code(200).message("成功").build());
+//        list.add(new ResponseMessageBuilder().code(100).message("请求参数有误").build());
+//        list.add(new ResponseMessageBuilder().code(101).message("未授权").build());
+//        list.add(new ResponseMessageBuilder().code(102).message("禁止访问").build());
+//        list.add(new ResponseMessageBuilder().code(103).message("请求路径不存在").build());
+//        list.add(new ResponseMessageBuilder().code(104).message("服务器内部错误").build());
+//        return list;
+//    }
+//}
