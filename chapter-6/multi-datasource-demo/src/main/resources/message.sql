1b3589575d633941ca6fdf93c79488bb3fb81c73
zhayh <zhayh.nj@gmail.com>
2020-03-30 21:40:49 +0800

    删除chapter-6

--- chapter-6/multi-datasource-demo/src/main/resources/message.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS message;
-CREATE TABLE message (
-  id bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  content varchar(32) DEFAULT NULL COMMENT '正文',
-  summary varchar(32) DEFAULT NULL COMMENT '摘要',
-  age  int DEFAULT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
