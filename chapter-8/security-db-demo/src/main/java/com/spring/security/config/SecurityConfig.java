9163b89936b26c4552d775ddc90bf441e133b337
zhayh <zhayh.nj@gmail.com>
2020-04-16 13:14:30 +0800

    Spring Security基于数据库认证的案例

--- chapter-8/security-db-demo/src/main/java/com/spring/security/config/SecurityConfig.java
+++ chapter-8/security-db-demo/src/main/java/com/spring/security/config/SecurityConfig.java
@@ -1,9 +1,43 @@
 package com.spring.security.config;
 
+import com.spring.security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 /**
  * @author : zhayh
  * @date : 2020-4-15 22:11
  * @description :
  */
-public class SecurityConfig {
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService userService;
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/dba/**").hasRole("dba")
+                .antMatchers("/admin/**").hasRole("admin")
+                .antMatchers("/user/**").hasRole("user")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .csrf().disable();
+    }
 }
--- chapter-8/security-db-demo/src/main/java/com/spring/security/controller/HelloController.java
+++ chapter-8/security-db-demo/src/main/java/com/spring/security/controller/HelloController.java
@@ -1,9 +1,29 @@
 package com.spring.security.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : zhayh
  * @date : 2020-4-15 22:14
  * @description :
  */
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello security";
+    }
+    @GetMapping("/dba/hello")
+    public String dba() {
+        return "hello dba";
+    }
+    @GetMapping("/admin/hello")
+    public String admin() {
+        return "hello admin";
+    }
+    @GetMapping("/user/hello")
+    public String user() {
+        return "hello user";
+    }
 }
