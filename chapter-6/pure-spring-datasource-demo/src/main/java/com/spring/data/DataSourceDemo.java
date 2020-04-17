3a9a4a9f31843800d36e6cd67eec1f0e59db9057
zhayh <zhayh.nj@gmail.com>
2020-04-17 08:15:09 +0800

    spring datasource的配置

---
+++ chapter-6/pure-spring-datasource-demo/src/main/java/com/spring/data/DataSourceDemo.java
@@ -0,0 +1,68 @@
+package com.spring.data;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @author : zhayh
+ * @date : 2020-2-7 16:22
+ * @description : 数据源Demo
+ */
+
+@Configuration
+@EnableTransactionManagement
+public class DataSourceDemo {
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) throws SQLException {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext*.xml");
+        showBeans(context);
+        dataSourceDemo(context);
+    }
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("driverClassName", "org.h2.Driver");
+        properties.setProperty("url", "jdbc:h2:mem:testdb");
+        properties.setProperty("username", "sa");
+        return BasicDataSourceFactory.createDataSource(properties);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() throws Exception {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    private static void showBeans(ApplicationContext applicationContext) {
+        System.out.println(Arrays.toString(applicationContext.getBeanDefinitionNames()));
+    }
+
+    private static void dataSourceDemo(ApplicationContext applicationContext) throws SQLException {
+        DataSourceDemo demo = applicationContext.getBean("dataSourceDemo", DataSourceDemo.class);
+        demo.showDataSource();
+    }
+
+    public void showDataSource() throws SQLException {
+        System.out.println(dataSource.toString());
+        Connection conn = dataSource.getConnection();
+        System.out.println(conn.toString());
+        conn.close();
+    }
+}
