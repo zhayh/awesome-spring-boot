45e17d478564556ab769a484726228a514dabe14
zhayh <zhayh.nj@gmail.com>
2020-04-25 20:16:45 +0800

    schedule的基本应用

---
+++ chapter-10/schedule-demo/src/test/java/com/spring/job/schedule/ScheduleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.job.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
