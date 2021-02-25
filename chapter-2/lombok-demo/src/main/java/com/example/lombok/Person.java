d4758ff76150a003cad270b93fd6420dc0e74edd
zhayh <zhayh.nj@gmail.com>
2021-02-25 09:31:19 +0800

    lombok案例

---
+++ chapter-2/lombok-demo/src/main/java/com/example/lombok/Person.java
@@ -0,0 +1,9 @@
+package com.example.lombok;
+
+/**
+ * @author : zhayh
+ * @date : 2021-2-24 22:13
+ * @description :
+ */
+public class Person {
+}
---
+++ chapter-2/lombok-demo/src/main/java/com/example/lombok/Student.java
@@ -0,0 +1,9 @@
+package com.example.lombok;
+
+/**
+ * @author : zhayh
+ * @date : 2021-2-24 22:27
+ * @description :
+ */
+public class Student {
+}
