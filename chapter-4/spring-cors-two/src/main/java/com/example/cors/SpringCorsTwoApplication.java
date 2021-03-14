446bdb9639697653351059e818c578e1674c9383
zhayh <zhayh.nj@gmail.com>
2021-03-14 08:30:47 +0800

    访问cors-one提供的跨域资源

---
+++ chapter-4/spring-cors-two/src/main/java/com/example/cors/SpringCorsTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCorsTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCorsTwoApplication.class, args);
+    }
+
+}
---
+++ chapter-4/spring-cors-two/src/test/java/com/example/cors/SpringCorsTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorsTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
