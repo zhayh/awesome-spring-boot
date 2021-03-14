44369774650b8e6f21a8252be8a62587ff0bd47b
zhayh <zhayh.nj@gmail.com>
2021-03-14 10:44:55 +0800

    @ControllerAdvice的全局异常处理

--- chapter-4/spring-controlleradvice/src/main/java/com/example/controlleradvice/GlobalConfig.java
+++ chapter-4/spring-controlleradvice/src/main/java/com/example/controlleradvice/GlobalConfig.java
@@ -2,8 +2,10 @@ package com.example.controlleradvice;
 
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,14 +18,33 @@ import java.util.Map;
 
 @ControllerAdvice
 public class GlobalConfig {
+    /**
+     * 全局异常处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    public ModelAndView customException(Exception e) {
+        ModelAndView mv = new ModelAndView("error");
+        mv.addObject("message", e.getMessage());
+        return mv;
+    }
+    /**
+     * 全局数据绑定
+     * @return
+     */
     @ModelAttribute(value = "info")
     public Map<String, String> userInfo() {
         Map<String, String> map = new HashMap<>();
-        map.put("username", "罗贯中");
+        map.put("username", "张三");
         map.put("gender", "男");
         return map;
     }
 
+    /**
+     * 全局数据预处理
+     * @param binder
+     */
     @InitBinder("book")
     public void initBook(WebDataBinder binder) {
         binder.setFieldDefaultPrefix("b.");
--- chapter-4/spring-controlleradvice/src/main/java/com/example/controlleradvice/TestController.java
+++ chapter-4/spring-controlleradvice/src/main/java/com/example/controlleradvice/TestController.java
@@ -3,19 +3,34 @@ package com.example.controlleradvice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 /**
  * @author : zhayh
- * @date : 2021-3-4 11:43
+ * @date : 2021-3-4 14:03
  * @description :
  */
 
 @Slf4j
 @RestController
-public class UserInfoController {
+public class TestController {
+    @GetMapping("/exception")
+    public String get()  {
+        throw new RuntimeException("发生异常");
+    }
+
+    @GetMapping("/book")
+    public String book(@ModelAttribute("book") Book book,
+                       @ModelAttribute("author") Author author) {
+        String result = book.toString() + ">>>" + author.toString();
+
+        log.info(result);
+        return result;
+    }
+
     @GetMapping("/userInfo")
     public String hello(Model model) {
         StringBuilder result = new StringBuilder();
@@ -27,3 +42,6 @@ public class UserInfoController {
         return result.toString();
     }
 }
+
+
+// http://localhost:8080/book?b.name=三国演义&b.price=69.9&a.name=罗贯中&a.age=85
